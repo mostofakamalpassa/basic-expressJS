33ca969dab5eb979fd887a938fc8370ae47e0696
mkpbd <mostofakamal1990@gmail.com>
2022-07-06 14:56:23 +0600

    Experess js basic  bootstraptin

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// common js  old way node js file required 
+ const express =require('express');
+
+ const app = express();
+
+
+// send html to browser 
+
+
+
+
+
+
+ app.listen(5000,()=>{
+    console.log('server is running')
+ })
+
+// // esm for file import 
+// import express from 'express';
